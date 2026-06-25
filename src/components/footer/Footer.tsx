e2c0a638222597bc559b7ec24b167db599e1f2fc
luana <luanamporto56@gmail.com>
2026-06-25 17:12:08 -0300

    Implementando Cadastro e Login

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react"
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from "@phosphor-icons/react"
 
 function Footer() {
 
@@ -9,13 +9,19 @@ function Footer() {
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                        Blog Pessoal Generation | Copyright: {data}
-                    </p>
+                            Blog Pessoal Generation | Copyright: {data}
+                        </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogoIcon size={48} weight='bold' />
-                        <InstagramLogoIcon size={48} weight='bold' />
-                        <FacebookLogoIcon size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/in/seu_usuario" target="_blank">
+                            <LinkedinLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/seu_usuario" target="_blank">
+                            <InstagramLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.facebook.com/seu_usuario" target="_blank">
+                            <FacebookLogo size={48} weight='bold' />
+                        </a>
                     </div>
                 </div>
             </div>
